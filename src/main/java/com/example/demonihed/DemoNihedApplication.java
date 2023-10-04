45a455ef40964e2293366e91cd468f07ba0a4eb5
Nihed ATTIA <nihed.attia@esprit.tn>
2023-10-05 00:32:18 +0200

    first commit

---
+++ src/main/java/com/example/demonihed/DemoNihedApplication.java
@@ -0,0 +1,13 @@
+package com.example.demonihed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNihedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoNihedApplication.class, args);
+    }
+
+}
